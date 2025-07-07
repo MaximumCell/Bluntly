b6aafa6d3873bded6ae7fca7f1ecceb7a63fc35d
MaximumCell <alihassanwinner@gmail.com>
2025-07-08 00:24:52 +0500

    users routes added

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { followUser, getCurrentUser, getUserProfile, syncUser, updateUserProfile } from "../controllers/user.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", getUserProfile);
+router.put("/profile", protectRoute, updateUserProfile);
+router.post("/sync", protectRoute, syncUser);
+router.post("/me", protectRoute, getCurrentUser);
+router.post("/follow/:targetUserId", protectRoute, followUser)
+
+export default router;
