6fa5cfbf9066db686ecaff0a08333e5d17a9b2bb
MaximumCell <alihassanwinner@gmail.com>
2025-07-08 20:35:40 +0500

    checked logout fn

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useClerk } from '@clerk/clerk-expo'
 
 const HomeScreen = () => {
-  
+  const { signOut} = useClerk();
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Text onPress={() => signOut()} className='text-blue-500'>Sign Out</Text>
+      <Text className='items-center justify-center '>Welcome to the Home Screen!</Text>
     </View>
   )
 }
