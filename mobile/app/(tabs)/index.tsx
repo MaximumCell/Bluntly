977689af6762f95a1e9f60789da2c21ef03acbee
MaximumCell <alihassanwinner@gmail.com>
2025-07-08 22:10:07 +0500

    tabs setup done

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SignOutButton from '@/components/SignOutButton'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Home Screen</Text>
+        <SignOutButton />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
