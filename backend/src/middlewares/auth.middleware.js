a8a3684a8ae3b3f9f0bfe6382c8ceda5ab1786be
MaximumCell <alihassanwinner@gmail.com>
2025-07-09 20:19:11 +0500

    now tried a new fix

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,6 +1,11 @@
+import { getAuth } from '@clerk/express';
+
 export const protectRoute = (req, res, next) => {
-    if (!req.auth().isAuthenticated()) {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
         return res.status(401).json({ message: "Unauthorized - you must be logged in" });
     }
+
     next();
 };
