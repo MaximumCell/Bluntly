b1bbd05b8d78acc06b6cdc1b1cc1b3d407d5ce4c
MaximumCell <alihassanwinner@gmail.com>
2025-07-26 14:47:27 +0500

    fix: update import paths in user controller and routes for consistency

--- socket-server/routes/user.routes.js
+++ socket-server/routes/user.routes.js
@@ -1,15 +1,12 @@
-import express from "express";
-import {
-  getAllUsers,
-  getUserByUsername,
-} from "../controllers/user.controller.js";
+import express from 'express';
+import { getAllUsers, getUserByUsername } from '../controllers/user';
 
 const router = express.Router();
 
 // GET /api/users - Get all users
-router.get("/", getAllUsers);
+router.get('/', getAllUsers);
 
 // GET /api/users/:username - Get user by username
-router.get("/:username", getUserByUsername);
+router.get('/:username', getUserByUsername);
 
 export default router;
